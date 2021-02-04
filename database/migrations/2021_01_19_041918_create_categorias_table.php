33687e990d678693cc390c683267831c43db53f4
cleicykerolim <cleicykerolim@gmail.com>
2021-02-03 23:10:30 -0300

    Migrations de categorias

--- database/migrations/2021_01_19_041918_create_categorias_table.php
+++ database/migrations/2021_01_19_041918_create_categorias_table.php
@@ -15,7 +15,8 @@ class CreateCategoriasTable extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('categoria');
+            $table->string('tipo');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
