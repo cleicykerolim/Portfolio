d345df9ab9afe9d4bf148c31c0b3ec49582e2b4d
cleicykerolim <cleicykerolim@gmail.com>
2021-02-03 23:21:27 -0300

    Models de categorias

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+    	
+    	'tipo',
+        'user_id'
+
+    ];
+
+    public function users(){
+
+    	return $this->belongsTo(User::class);
+    	
+    }
+
+    public function produtos(){
+
+    	return $this->hasMany(Produto::class);
+ 
+    }
 }
+
