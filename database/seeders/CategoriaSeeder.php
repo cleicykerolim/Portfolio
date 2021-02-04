8e0170e50bb97b860f69732f9b16f89a963c9f39
cleicykerolim <cleicykerolim@gmail.com>
2021-02-03 23:18:15 -0300

    Seeders de categorias

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Categoria;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class CategoriaSeeder extends Seeder
 {
