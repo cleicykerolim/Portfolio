3b7859e2c7d2c60f951860a068df58c4797918e4
cleicykerolim <cleicykerolim@gmail.com>
2021-02-05 23:58:11 -0300

    Listagem de projetos e categorias no dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\HTTP\Controllers\HomeController;
+// use App\HTTP\Controllers\HomeController;
+use App\HTTP\Controllers\ProjetoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [HomeController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+	return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::resource('projetos', \App\HTTP\Controllers\ProjetoController::class);
+Route::post('/projetos/novo', [ProjetoController::class, 'store'])->name('add-projeto');
+
+// Route::get('/dashboard', [HomeController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+
+// Route::resource('projetos', \App\HTTP\Controllers\ProjetoController::class);
 
 require __DIR__.'/auth.php';
